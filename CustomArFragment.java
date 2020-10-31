430b604a4b58aa5f8a035ada2df3c1b5fe7915bd
manasyadav1070 <60380686+manasyadav1070@users.noreply.github.com>
2020-10-31 23:23:16 +0530

    Add files via upload

---
+++ CustomArFragment.java
@@ -0,0 +1,28 @@
+package com.example.arencyclopedia;
+
+import android.widget.Toast;
+
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class CustomArFragment extends ArFragment {
+    private String end = "end";
+
+    @Override
+    protected Config getSessionConfiguration(Session session) {
+        Config config = new Config(session);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        config.setFocusMode(Config.FocusMode.AUTO);
+        session.configure(config);
+
+        this.getArSceneView().setupSession(session);
+
+
+        ((MainActivity)getActivity()).setupDatabase(config,session);
+
+        return config;
+
+
+    }
+}
